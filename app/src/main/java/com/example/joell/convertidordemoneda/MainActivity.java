daefd1446762e9a46a7ff1d6c42bb2f07e5ac778
morafer <fercho2296@gmail.com>
2019-04-09 14:24:21 -0600

    Agregando Boton y Spin para la conversion

--- app/src/main/java/com/example/joell/convertidordemoneda/MainActivity.java
+++ app/src/main/java/com/example/joell/convertidordemoneda/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.joell.convertidordemoneda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button temperatura =null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        temperatura=(Button)findViewById(R.id.btn1);
+        temperatura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cambiar = Intent(getApplicationContext(), Temperatura.class);
+                startActivity(cambiar);
+            }
+        });
+
+
     }
 }
